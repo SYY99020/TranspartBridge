3e30baadd8303931d3af3e3bff59ef54520c03dc
xiaogong <1209457106@qq.com>
2019-12-31 22:44:34 +0800

    add ui(PC)

--- TransparentBridge/Initial.cpp
+++ TransparentBridge/Initial.cpp
@@ -1,9 +1,9 @@
 #include "header.h"
-
+#include <QDebug>
 int read_config(){
     ifstream ifile(CONFIG_PATH, ios::in);
     if (!ifile){
-        qDebug() << "打开文件失败！" << endl;
+        qDebug() << "打开文件失败！";
         exit(-1);
     }
 
